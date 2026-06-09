fc1f207835c890139d571c015b9c62a1a055de4d
Dreacofred <Diego.estacionbc@gmail.com>
2026-06-09 18:58:01 -0300

    Update lector.py

--- lector.py
+++ lector.py
@@ -1050,6 +1050,6 @@ elif opcion == "Gestión de Clientes":
 # 6. MÓDULO: LABORATORIO IA (COBRANZAS)
 # ==========================================
         elif opcion == "Laboratorio IA":
-        # Leemos y ejecutamos el archivo directamente
-        with open("bot.py", encoding="utf-8") as f:
-            exec(f.read())
+            # Leemos y ejecutamos el archivo directamente
+            with open("bot.py", encoding="utf-8") as f:
+                exec(f.read())
